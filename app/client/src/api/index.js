fa079da6e69a42a3ac29113f070287967a6a87cf
nhan2892005 <143471539+nhan2892005@users.noreply.github.com>
2024-09-27 22:56:04 +0700

    update api index.js

--- app/client/src/api/index.js
+++ app/client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://analystreviewproduct.onrender.com/' });
 
 export const getProductsbySearch = (query) => API.get('products/search', { params: { query } });
 
